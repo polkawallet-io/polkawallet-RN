492b98934443a161e165c2d9c0d720149c29803e
Fuyao-Jiang <three3body@gmail.com>
2019-01-10 19:26:09 +0800

    Assets_Page

--- node_modules/react-navigation-stack/dist/utils/GestureContext.js
+++
@@ -1,4 +0,0 @@
-import React from 'react';
-
-const GestureContext = React.createContext(null);
-export default GestureContext;
--- node_modules/react-navigation/NavigationTestUtils.js
+++
@@ -1,7 +0,0 @@
-import { _TESTING_ONLY_reset_container_count } from './src/createNavigationContainer';
-
-export default {
-  resetInternalState: () => {
-    _TESTING_ONLY_reset_container_count();
-  },
-};
