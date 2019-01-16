b6153f831c119bfcdc6b2391fe0fde1abbedb81f
zx870076178 <870076178@qq.com>
2019-01-16 17:57:45 +0800

    side_menue&CA

--- App.js
+++ App.js
@@ -5,9 +5,11 @@ import {
 } from 'react-native';
 import {StackNavigator} from 'react-navigation'
 import Tabbed_Navigation from './src/code/Tabbed_Navigation.js'
-
+import right_menu from './src/code/Assetes/secondary/right_menu'
+import Create_Account from './src/code/Assetes/secondary/Create_Account'
 const Polkawallet_App =  StackNavigator({
   Tabbed_Navigation:{screen:Tabbed_Navigation,navigationOptions:{header:null}},
+  Create_Account:{screen:Create_Account,navigationOptions:{header:null}},
  })
 export default class Polkawallet extends Component {
   render() {
--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 
 import {AppRegistry} from 'react-native';
 import App from './App';
-import Shiyan from './src/code/Assetes/secondary/right_menu'
+import Shiyan from './src/code/Assetes/secondary/shiyan'
 import {name as appName} from './app.json';
 
 AppRegistry.registerComponent(appName, () => App);
