94f13e191ff9d48aa7fb5b355a30a7e2063c3bd4
Fuyao-Jiang <three3body@gmail.com>
2019-01-17 14:43:40 +0800

    import-api

--- App.js
+++ App.js
@@ -5,6 +5,13 @@ import {
 } from 'react-native';
 import {StackNavigator} from 'react-navigation'
 import Tabbed_Navigation from './src/code/Tabbed_Navigation.js'
+import Api from '@polkadot/api/promise';
+import WsProvider from '@polkadot/rpc-provider/ws';
+import { Balance, BlockNumber } from '@polkadot/types';
+
+const { Keyring } = require('@polkadot/keyring');
+const { stringToU8a } = require('@polkadot/util');
+
 
 const Polkawallet_App =  StackNavigator({
   Tabbed_Navigation:{screen:Tabbed_Navigation,navigationOptions:{header:null}},
