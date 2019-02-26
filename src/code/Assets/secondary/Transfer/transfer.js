6d5fd8620b2dbb03a95a272d7701716ba001ac9f
zx870076178 <870076178@qq.com>
2019-02-26 18:03:42 +0800

    fix_transfer

--- src/code/Assets/secondary/Transfer/transfer.js
+++ src/code/Assets/secondary/Transfer/transfer.js
@@ -93,6 +93,7 @@ import {
         else{
             this.props.rootStore.stateStore.value=this.state.value*this.state.multiple
             this.props.rootStore.stateStore.inaddress=(this.props.rootStore.stateStore.isaddresses==0&&this.props.rootStore.stateStore.iscamera==0)?this.state.address:this.props.rootStore.stateStore.t_address
+            this.props.rootStore.stateStore.isaddresses=0
             this.props.rootStore.stateStore.transfer_address=0
             this.props.rootStore.stateStore.iscamera=0
             this.props.navigation.navigate('Make_transfer')
