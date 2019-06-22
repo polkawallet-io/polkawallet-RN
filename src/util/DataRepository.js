ea9c3cb4b86deeb9c081e233a1b4c25ec6cba800
jiangfuyao <three3body@gmail.com>
2019-06-23 01:56:48 +0800

    Add file header declaration

--- src/util/DataRepository.js
+++ src/util/DataRepository.js
@@ -1,7 +1,19 @@
+/*
+ * @Description: COPYRIGHT © 2018 POLKAWALLET (HK) LIMITED 
+ *  This file is part of Polkawallet. 
+ 
+ It under the terms of the GNU General Public License as published by 
+ the Free Software Foundation, either version 3 of the License. 
+ You should have received a copy of the GNU General Public License 
+ along with Polkawallet. If not, see <http://www.gnu.org/licenses/>. 
+
+ * @Autor: POLKAWALLET LIMITED
+ * @Date: 2019-06-18 21:08:00
+ */
 import { AsyncStorage } from 'react-native'
 
 /**
- * 数据持久化类
+ * 数据持久化类 | Data persistence class
  */
 export default class DataRepository {
   /**
@@ -40,7 +52,7 @@ export default class DataRepository {
   }
 
   /**
-   * 移除本地存存的key
+   * 移除本地存的key
    * @param key
    * @returns {*}
    */
@@ -51,7 +63,7 @@ export default class DataRepository {
   }
 
   /**
-   * 移除本地存存的keys
+   * 移除本地存的keys
    * @param keys
    * @returns {*}
    */
