50d354d1637b68a3e1dcce83aa5b04c711603788
Fuyao-Jiang <three3body@gmail.com>
2019-07-06 16:53:44 +0800

    disableYellowBox

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ console.ignoredYellowBox = [
   'Invalid props.style key'
 ]
 
-console.disableYellowBox = false
+console.disableYellowBox = true
 ;(async () => {
   const provider = new WsProvider(AppState.stateStore.ENDPOINT)
   const api = await Api.create(provider)
