e4550de12198d4d3f330a728654fb417f30085f2
Michael <michael.zhai@centrality.ai>
2019-07-19 22:33:51 +1200

    Add wasm-crypto to babel rewrite

--- babel.config.js
+++ babel.config.js
@@ -1,21 +1,3 @@
 module.exports = {
-  presets: ['module:metro-react-native-babel-preset'],
-  plugins: [
-    [
-      'rewrite-require',
-      {
-        aliases: {
-          crypto: 'react-native-crypto',
-          stream: 'readable-stream',
-          _stream_duplex: 'readable-stream/duplex',
-          _stream_passthrough: 'readable-stream/passthrough',
-          _stream_readable: 'readable-stream/readable',
-          _stream_transform: 'readable-stream/transform',
-          _stream_writable: 'readable-stream/writable',
-          vm: 'vm-browserify'
-        },
-        throwForNonStringLiteral: true
-      }
-    ]
-  ]
+  presets: ['module:metro-react-native-babel-preset']
 }
