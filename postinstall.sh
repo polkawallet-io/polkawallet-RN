384037735609469baaae10b1b99fdd3e9d2afab4
Michael <michael.zhai@centrality.ai>
2019-08-05 11:25:25 +1200

    Hacking NODE_ARGS to add extra memory

--- postinstall.sh
+++ postinstall.sh
@@ -1,2 +1,5 @@
 # code injection
 sed -i -e $'s/var randomBytes = require(\'randombytes\')*/var randomBytes = require(\'crypto\').randomBytes/g' ./node_modules/bip39/index.js
+
+# add extra memory to node env
+sed -i -e $'s/NODE_ARGS=""/NODE_ARGS="--max_old_space_size=4096"/g' ./node_modules/react-native/scripts/react-native-xcode.sh
